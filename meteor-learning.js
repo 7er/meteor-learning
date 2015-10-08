9527dd40892719eeb80cf6392d919128258b3f15
Syver Enstad <syver.enstad@gmail.com>
2015-10-08 19:43:08 +0200

    Initialize counter to zero not present

--- meteor-learning.js
+++ meteor-learning.js
@@ -1,12 +1,13 @@
 Counters = new Mongo.Collection("counters")
 
+
 if (Meteor.isClient) {
   // counter starts at 0
   // Session.setDefault('counter', 0);
 
   Template.hello.helpers({
     counter: function () {
-      return Counters.findOne({ _id: 'jSh2HhsKbpAmociDY'}).a;
+      return Counters.findOne({_id: "semsket"}).a;
     }
   });
 
@@ -14,14 +15,17 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
         Counters.update(
-            { _id: 'jSh2HhsKbpAmociDY'},
+            {_id: "semsket"},
             {$inc: {a: 2}});
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+    Meteor.startup(function () {
+        counterOrNull = Counters.findOne({_id: "semsket"});
+        if (!counterOrNull) {
+            Counters.insert({_id: "semsket", a: 0});
+        }
   });
 }
