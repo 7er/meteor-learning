c7b2b2aa0527b906b8c0a0fe666c2333f24a3484
Syver Enstad <syver.enstad@gmail.com>
2015-10-08 20:08:30 +0200

    Move code out of client/server specific blocks
    Just to see if that works :)

--- meteor-learning.js
+++ meteor-learning.js
@@ -1,31 +1,36 @@
 Counters = new Mongo.Collection("counters")
 
+function counter() {
+    return Counters.findOne({_id: "semsket"}).a;
+}
+
+function updateCounter() {
+    Counters.update(
+        {_id: "semsket"},
+        {$inc: {a: 2}});
+}
+
+function initCounter() {
+    counterOrNull = Counters.findOne({_id: "semsket"});
+    if (!counterOrNull) {
+        Counters.insert({_id: "semsket", a: 0});
+    }
+}
 
 if (Meteor.isClient) {
   // counter starts at 0
   // Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Counters.findOne({_id: "semsket"}).a;
-    }
-  });
+    Template.hello.helpers({
+      counter: counter
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-        Counters.update(
-            {_id: "semsket"},
-            {$inc: {a: 2}});
-    }
-  });
+    Template.hello.events({
+        // increment the counter when button is clicked       
+        'click button': updateCounter
+    });
 }
 
 if (Meteor.isServer) {
-    Meteor.startup(function () {
-        counterOrNull = Counters.findOne({_id: "semsket"});
-        if (!counterOrNull) {
-            Counters.insert({_id: "semsket", a: 0});
-        }
-  });
+    Meteor.startup(initCounter);
 }
