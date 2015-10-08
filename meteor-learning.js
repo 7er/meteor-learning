58ecb50b9d0cf8235ba2e00e7567cdaf82ab455a
Syver Enstad <syver.enstad@gmail.com>
2015-10-08 19:21:29 +0200

    Move data storage to Mongo on server

--- meteor-learning.js
+++ meteor-learning.js
@@ -1,17 +1,21 @@
+Counters = new Mongo.Collection("counters")
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  // Session.setDefault('counter', 0);
 
   Template.hello.helpers({
     counter: function () {
-      return Session.get('counter');
+      return Counters.findOne({ _id: 'jSh2HhsKbpAmociDY'}).a;
     }
   });
 
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+        Counters.update(
+            { _id: 'jSh2HhsKbpAmociDY'},
+            {$inc: {a: 2}});
     }
   });
 }
